73cd580ecaeedbb5b727f4e10fbaa123cfaef088
mjonesy <jonesy0007@gmail.com>
2016-09-29 16:49:48 -0500

    added link validations

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,7 +1,12 @@
 class VisitsController < ApplicationController
   def create
-    @visit = Visit.create
+    @link = Link.find_by(slug: params[:slug])
+
+    @visit = Visit.create(
+                          link_id: @link.id,
+                          times_viewed: self.times_viewed + 1
+                          )
     
-    redirect_to ''
+    redirect_to "http://#{@link.target_url}"
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :user
   has_many :visits
+
+  validates :slug, presence: true
+  validates :target_url, presence: true
 end
